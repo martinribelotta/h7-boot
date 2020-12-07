4d149f7c37bca54af2c2f97fd21c82162ae08f3e
Martin Ribelotta <martinribelotta@gmail.com>
2020-12-06 21:11:44 -0300

    Refactor emfat_disk.c
    Signed-off-by: Martin Ribelotta <martinribelotta@gmail.com>

--- Core/Src/emfat_disk.c
+++ Core/Src/emfat_disk.c
@@ -2,41 +2,66 @@
 
 emfat_t emfat;
 
-#define CMA_TIME EMFAT_ENCODE_CMA_TIME(1,12,2020, 13,0,0)
-#define CMA { CMA_TIME, CMA_TIME, CMA_TIME }
+#define CMA_TIME EMFAT_ENCODE_CMA_TIME(1, 12, 2020, 13, 0, 0)
+#define CMA                                                                                                            \
+    {                                                                                                                  \
+        CMA_TIME, CMA_TIME, CMA_TIME                                                                                   \
+    }
+#define ROOT                                                                                                           \
+    {                                                                                                                  \
+        "", true, 0, 0, 0, 0, 0, CMA, NULL, NULL                                                                       \
+    }
+#define ARRAY_FILE(n, lvl, off, a)                                                                                     \
+    {                                                                                                                  \
+        n, false, lvl, off, sizeof(a) - 1, sizeof(a) - 1, (size_t)a, CMA, rofile_read_proc, NULL                       \
+    }
+#define BIN_DYN_FILE(n, lvl, off, rd, wr)                                                                              \
+    {                                                                                                                  \
+        n, false, lvl, off, 0, 0, 0, CMA, rd, wr                                                                       \
+    }
+#define ENDROOT                                                                                                        \
+    {                                                                                                                  \
+        NULL                                                                                                           \
+    }
 
-#define DECL_READ_PROC(fname) void fname##_read_proc(uint8_t *dest, int size, uint32_t offset, size_t userdata)
-#define IMPL_READ_PROC(fname, array) DECL_READ_PROC(fname) { \
-    int len = 0; \
-	if (offset > sizeof(array)) return; \
-	len = (offset + size > sizeof(array))? sizeof(array) - offset : size; \
-	memcpy(dest, &array[offset], len); \
-}
-#define MAKE_RO_FILE_STR(fname, content) \
-static const uint8_t fname##_array[] = { content }; \
-static const int fname##_SIZE = sizeof(fname##_array) - 1; \
-void fname##_read_proc(uint8_t *dest, int size, uint32_t offset, size_t userdata) { \
-    int len = 0; \
-	if (offset > fname##_SIZE) return; \
-	len = (offset + size > sizeof(fname##_array))? fname##_SIZE - offset : size; \
-	memcpy(dest, &fname##_array[offset], len); \
-}
+static const uint8_t info_uf2_data[] = "UF2 Bootloader v1.1.3 SFA\r\n"
+                                       "Model: STM32H7 Minitor\r\n"
+                                       "Board-ID: STM32H7 Generic\r\n"
+                                       "\r\n"
+                                       "Compiled at: " __DATE__ " " __TIME__ "\r\n"
+                                       "With GCC " __VERSION__ "\r\n";
 
-MAKE_RO_FILE_STR(INFO_UF2, "Content of file info_uf2.txt");
-MAKE_RO_FILE_STR(INDEX, "Content of index.htm");
-MAKE_RO_FILE_STR(CURRENT, "Content of current.uf2");
+static const uint8_t index_data[] = "<body>"
+                                    "<h1>Generic STM32 Bootloader</h1>"
+                                    "<p>See more info in: <a href=\"https://github.com/martinribelotta/h7-boot\">"
+                                    "https://github.com/martinribelotta/h7-boot</a></p>"
+                                    "</body>";
 
-static emfat_entry_t entries[] =
+void rofile_read_proc(uint8_t *dest, int size, uint32_t offset, size_t userdata)
 {
-	// name           dir    lvl off size           max_size       user time read                write
-	{ "",             true,  0,  0,  0,             0,             0,   CMA, NULL,               NULL },
-	{ "INFO_UF2.TXT", false, 1,  0,  INFO_UF2_SIZE, INFO_UF2_SIZE, 0,   CMA, INFO_UF2_read_proc, NULL },
-	{ "INDEX.HTM",    false, 1,  0,  INDEX_SIZE,    INDEX_SIZE,    0,   CMA, INDEX_read_proc,    NULL },
-	{ "CURRENT.UF2",  false, 1,  0,  CURRENT_SIZE,  CURRENT_SIZE,  0,   CMA, CURRENT_read_proc,  NULL },
-	{ NULL }
+    uint8_t *src = (uint8_t *)userdata;
+    memcpy(dest, src + offset, size);
+}
+
+void CURRENT_read_proc(uint8_t *dest, int size, uint32_t offset, size_t userdata)
+{
+    memset(dest, '?', size);
+}
+
+void CURRENT_write_proc(const uint8_t *dest, int size, uint32_t offset, size_t userdata)
+{
+}
+
+static emfat_entry_t entries[] = {
+    ROOT,                                                                     //
+    ARRAY_FILE("INFO_UF2.TXT", 1, 0, info_uf2_data),                          //
+    ARRAY_FILE("INDEX.HTM", 1, 0, index_data),                                //
+    BIN_DYN_FILE("CURRENT.UF2", 1, 0, CURRENT_read_proc, CURRENT_write_proc), //
+    ENDROOT                                                                   //
 };
 
 void EMFATDisk_Init(void)
 {
+    entries[3].curr_size = entries[3].max_size = 4096*1024;
     emfat_init(&emfat, "emfat", entries);
 }
